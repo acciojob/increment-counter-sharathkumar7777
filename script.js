90f6535355e2c3daca7b7b8dcddcfc0db58a4970
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-25 13:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+
+let count = 0;
+
+    const counterEl = document.getElementById("counter");
+    const incrementBtn = document.getElementById("incrementBtn");
+
+    incrementBtn.addEventListener("click", function () {
+      // Show alert with un-incremented value
+      alert(count);
+
+      // Increment counter
+      count++;
+
+      // Update UI
+      counterEl.textContent = count;
+    });
